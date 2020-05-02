2a0436546ff2e519ba253b7665d1a6b5c08b31c9
UmmeFarwa <ufarwa872@gmail.com>
2020-05-02 18:20:29 +0500

    initial commit

---
+++ Assignment 3/js/index.js
@@ -0,0 +1,48 @@
+AOS.init();
+// number count for stats, using jQuery animate
+
+$('.counting').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+  
+    {
+  
+      duration: 3000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+       
+      }
+  
+    });  
+    
+  
+  });
+  //for gallery
+  baguetteBox.run('.tz-gallery');
+
+  //Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 0; 
+}
